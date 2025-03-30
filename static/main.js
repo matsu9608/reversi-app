017ff9ea550d399e93f3c2cf21f168efa2e8c27e
matsuyoshi <matsu1637@gmail.com>
2025-03-30 15:47:45 +0900

    フロント側でのエラーハンドリング処理調整

--- static/main.js
+++ static/main.js
@@ -1,49 +1,69 @@
-const EMPTY = 0
-const DARK = 1
-const LIGHT = 2
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
 
-const boardElement = document.getElementById('board')
+const boardElement = document.getElementById("board");
+const nextDiscMessageElement = document.getElementById("next-disc-message")
 
 async function showBoard(turnCount) {
-  const response = await fetch(`/api/games/latest/turns/${turnCount}`)
-  const responseBody = await response.json()
-  const board = responseBody.board
-  const nextDisc = responseBody.nextDisc
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
+  const responseBody = await response.json();
+  const board = responseBody.board;
+  const nextDisc = responseBody.nextDisc;
+
+  showNextDiscMessage(nextDisc)
 
   while (boardElement.firstChild) {
-    boardElement.removeChild(boardElement.firstChild)
+    boardElement.removeChild(boardElement.firstChild);
   }
 
   board.forEach((line, y) => {
     line.forEach((square, x) => {
       // <div class="square">
-      const squareElement = document.createElement('div')
-      squareElement.className = 'square'
+      const squareElement = document.createElement("div");
+      squareElement.className = "square";
 
       if (square !== EMPTY) {
         // <div class="stone dark">
-        const stoneElement = document.createElement('div')
-        const color = square === DARK ? 'dark' : 'light'
-        stoneElement.className = `stone ${color}`
+        const stoneElement = document.createElement("div");
+        const color = square === DARK ? "dark" : "light";
+        stoneElement.className = `stone ${color}`;
 
-        squareElement.appendChild(stoneElement)
+        squareElement.appendChild(stoneElement);
       } else {
-        squareElement.addEventListener('click', async () => {
-          const nextTurnCount = turnCount + 1
-          await registerTurn(nextTurnCount, nextDisc, x, y)
-          await showBoard(nextTurnCount)
-        })
+        squareElement.addEventListener("click", async () => {
+          const nextTurnCount = turnCount + 1;
+          const registerTurnResponce = await registerTurn(
+            nextTurnCount,
+            nextDisc,
+            x,
+            y
+          );
+
+          if (registerTurnResponce.ok) {
+            await showBoard(nextTurnCount);
+          }
+        });
       }
 
-      boardElement.appendChild(squareElement)
-    })
-  })
+      boardElement.appendChild(squareElement);
+    });
+  });
+}
+
+function showNextDiscMessage(nextDisc){
+  if (nextDisc){
+    const color = nextDisc === DARK ? '黒':'白' 
+    nextDiscMessageElement.innerText = `次は${color}の番です`
+  }else{
+    nextDiscMessageElement.innerText = ``
+  }
 }
 
 async function registerGame() {
-  await fetch('/api/games', {
-    method: 'POST'
-  })
+  await fetch("/api/games", {
+    method: "POST",
+  });
 }
 
 async function registerTurn(turnCount, disc, x, y) {
@@ -52,22 +72,22 @@ async function registerTurn(turnCount, disc, x, y) {
     move: {
       disc,
       x,
-      y
-    }
-  }
-
-  await fetch('/api/games/latest/turns', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+      y,
     },
-    body: JSON.stringify(requestBody)
-  })
+  };
+
+  return await fetch("/api/games/latest/turns", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  });
 }
 
 async function main() {
-  await registerGame()
-  await showBoard(0)
+  await registerGame();
+  await showBoard(0);
 }
 
-main()
+main();
